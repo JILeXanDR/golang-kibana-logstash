0198fc2ffe11102845b7163626c2ba677b1107ff
Alexandr <jilexandr@gmail.com>
2019-03-04 15:51:01 +0200

    first commit

---
+++ logger.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"gopkg.in/gemnasium/logrus-graylog-hook.v2"
+)
+
+// New creates new logger
+func NewLogger(logstashServer string) *logrus.Logger {
+	logger := logrus.New()
+
+	logger.SetLevel(logrus.TraceLevel)
+
+	hook := graylog.NewGraylogHook(logstashServer, map[string]interface{}{})
+	logger.AddHook(hook)
+
+	return logger
+}
